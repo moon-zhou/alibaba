08bdcec5075c42de6d2da8d5117bb5a60a6139a2
moon-zhou <ayimin1989@163.com>
2020-12-26 18:22:39 +0800

    feat: nacos学习
    1. spring cloud 整合服务注册
    1. 服务启动成功，但是未注册成功，后续解决，提示：java.lang.IllegalArgumentException: Param 'serviceName' is illegal, serviceName is blank

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ learning project about alibaba open source framework
 
 ### 阿里巴巴开源框架学习
 1. nacos
+    * nacos学习整理目录
+    * 纯jdk操作nacos示例
+1. nacos-spring-cloud
+    * spring cloud 下使用nacos示例集合
---
+++ nacos-spring-cloud/nacos-spring-cloud-discovery/nacos-spring-cloud-provider/src/main/java/org/moonzhou/alibaba/learning/nacos/spring/cloud/discovery/provider/NacosProviderApplication.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C), 2002-2020, moon-zhou
+ * FileName: NacosProviderApplication.java
+ * Author:   moon-zhou
+ * Email:    ayimin1989@163.com
+ * Date:     2020/12/23 15:40
+ * Description: //模块目的、功能描述
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名    修改时间    版本号       描述
+ */
+package org.moonzhou.alibaba.learning.nacos.spring.cloud.discovery.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 功能描述:<br>
+ *
+ * @author moon-zhou
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+}
+
