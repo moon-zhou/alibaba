20140bb7cef54b9581e99f3bce8e8cfd640d4e3f
moon-zhou <ayimin1989@163.com>
2020-12-28 18:21:48 +0800

    feat: nacos学习
    配置消费者

---
+++ nacos-spring-cloud/nacos-spring-cloud-discovery/nacos-spring-cloud-consumer/src/main/java/org/moonzhou/alibaba/learning/nacos/spring/cloud/discovery/consumer/NacosConsumerApplication.java
@@ -0,0 +1,55 @@
+package org.moonzhou.alibaba.learning.nacos.spring.cloud.discovery.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 功能描述:<br>
+ *
+ * @author moon-zhou
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConsumerApplication {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(org.moonzhou.alibaba.learning.nacos.spring.cloud.discovery.consumer.NacosConsumerApplication.class, args);
+    }
+
+    @RestController
+    public class TestController {
+
+        private final RestTemplate restTemplate;
+
+        @Autowired
+        public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+        /**
+         * http://localhost:8080/echo/123
+         * @param str
+         * @return
+         */
+        @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+        public String echo(@PathVariable String str) {
+            // 调用服务
+            return restTemplate.getForObject("http://moon-provider/echo/" + str, String.class);
+        }
+    }
+}
